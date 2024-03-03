1a374ab4275291516e5168dfd939b3e3b2f47140
rcuetof <rcuetof@gmail.com>
2024-03-03 02:03:58 -0500

    x

--- assets/tab-container.js
+++ assets/tab-container.js
@@ -1,18 +1,16 @@
 function changeTab(tabNumber) {
     // Obtiene el contenido de la pestaña seleccionada
     var selectedTabContent = document.getElementById("content" + tabNumber);
+    
     // Verifica si el contenido está visible
     var isTabVisible = window.getComputedStyle(selectedTabContent).display !== "none";
+    
     // Oculta todos los contenidos
     var tabContents = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].style.display = "none";
     }
-    // Muestra el contenido de la pestaña seleccionada
-    var selectedTabContent = document.getElementById("content" + tabNumber);
-    if (selectedTabContent) {
-        selectedTabContent.style.display = "block";
-    }
+    
     // Muestra el contenido de la pestaña seleccionada solo si no estaba visible previamente
     if (!isTabVisible) {
         selectedTabContent.style.display = "block";
@@ -41,4 +39,3 @@ tabContainer.addEventListener("mousemove", function (event) {
         initialX = event.clientX;
     }
 });
-
