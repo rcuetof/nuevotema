bf2cf023d01f3cd38b1371eff4ca12e7a23b8292
rcuetof <rcuetof@gmail.com>
2024-03-05 20:00:58 -0500

    x

--- assets/tab-container.js
+++ assets/tab-container.js
@@ -40,16 +40,6 @@ tabContainer.addEventListener("mousemove", function (event) {
     }
 });
 
-
-
-
-
-
-
-
-
-
-
 function Changeevent(EventNumber) {
     // Obtiene el contenido del evento seleccionado
     var selectedEventContent = document.getElementById("event" + EventNumber);
